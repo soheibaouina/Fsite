1a4d6478f8104db94f16ba61c974ff3edf244d31
soheib <aouina.soheib.study@gmail.com>
2025-10-14 13:18:56 +0100

    add login page and signin page

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request , 'accounts\home.html', {'name': 'John Doe'})
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
