bf9c2d7827692508c41f3b67924d42944eaf3ba3
soheib <aouina.soheib.study@gmail.com>
2025-10-14 12:07:52 +0100

    #3 commit

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the Accounts Home Page")
+    return render(request , 'accounts\home.html', {'name': 'John Doe'})
